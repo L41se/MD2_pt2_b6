4655a098956a850fa4355028e6654233f71adc59
L41se <manh.kien.21122012@gmail.com>
2023-10-17 13:58:19 +0700

    bob

---
+++ Ke_thua_hinh_hoc/CircleComparator.java
@@ -0,0 +1,13 @@
+package Ke_thua_hinh_hoc;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
